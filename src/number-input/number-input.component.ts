bb15f3fa21e49cceec9bad5a6b9650c240b80c2a
dsalazarl <dsalazarl@gmail.com>
2023-02-14 14:48:02 -0300

    added props and changed style behavior

--- src/number-input/number-input.component.ts
+++ src/number-input/number-input.component.ts
@@ -7,8 +7,16 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./number-input.component.css'],
 })
 export class NumberInputComponent implements OnInit {
+  @Input() value: number;
+  @Input() onChange: number;
+
   @Input() min?: number;
   @Input() max?: number;
+  @Input() softMax?: number;
+  @Input() softMin?: number;
+  @Input() step?: number = 1;
+  @Input() editable?: boolean;
+  @Input() precision?: number;
 
   constructor() {}
 
